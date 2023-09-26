7df3e149109805b2d3294dd82338f17a4b73e275
nivi1234567890 <144796285+nivi1234567890@users.noreply.github.com>
2023-09-26 17:01:39 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,8 +1,16 @@
-n = 23
-fact = 1
- 
-for i in range(1, n+1):
-    fact = fact * i
- 
-print("The factorial of 23 is : ", end="")
-print(fact)
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
